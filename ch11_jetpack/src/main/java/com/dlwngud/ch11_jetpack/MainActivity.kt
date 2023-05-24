bf417508719302e9b903b3bc19ac661d280bcf9a
dlwngud <wngud980411@naver.com>
2023-05-25 00:16:56 +0900

    ch11_제트팩 라이브러리 MenuItem 객체를 얻고 그 안에 포함된 ActionView 객체 획득

--- ch11_jetpack/src/main/java/com/dlwngud/ch11_jetpack/MainActivity.kt
+++ ch11_jetpack/src/main/java/com/dlwngud/ch11_jetpack/MainActivity.kt
@@ -2,6 +2,10 @@ package com.dlwngud.ch11_jetpack
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.SearchView
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
@@ -46,5 +50,40 @@ class MainActivity : AppCompatActivity() {
         )
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         toggle.syncState()
+
+        // 뷰 페이저에 어댑터 적용
+        val adapter = MyFragmentPagerAdapter(this)
+        binding.viewPager.adapter = adapter
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.menu_main, menu)
+
+        // MenuItem 객체를 얻고 그 안에 포함된 ActionView 객체 획득
+        val menuItem = menu?.findItem(R.id.menu_search)
+        val searchView = menuItem?.actionView as SearchView
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                // 키보드의 검색 버튼을 클릭한 순간의 이벤트
+                Log.d("kkang", "search text : $query")
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                // 검색어 변경 이벤트
+                return true
+            }
+
+        })
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // 이벤트가 토글 버튼에 발생하면
+        if (toggle.onOptionsItemSelected(item)) {
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
