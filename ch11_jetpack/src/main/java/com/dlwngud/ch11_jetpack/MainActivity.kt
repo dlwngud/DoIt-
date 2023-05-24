a50cc5a0d7f6dff87000ff6c83827626f8655873
dlwngud <wngud980411@naver.com>
2023-05-25 00:06:28 +0900

    ch11_제트팩 라이브러리 뷰 페이저 어댑터

--- ch11_jetpack/src/main/java/com/dlwngud/ch11_jetpack/MainActivity.kt
+++ ch11_jetpack/src/main/java/com/dlwngud/ch11_jetpack/MainActivity.kt
@@ -2,9 +2,32 @@ package com.dlwngud.ch11_jetpack
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.dlwngud.ch11_jetpack.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var toggle: ActionBarDrawerToggle
+
+    // 뷰 페이저 어댑터
+    class MyFragmentPagerAdapter(activity:FragmentActivity): FragmentStateAdapter(activity){
+        val fragments: List<Fragment>
+        init {
+            fragments = listOf(OneFragment(), TwoFragment(), ThreeFragment())
+        }
+
+        override fun getItemCount(): Int {
+            return fragments.size
+        }
+
+        override fun createFragment(position: Int): Fragment {
+            return fragments[position]
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
