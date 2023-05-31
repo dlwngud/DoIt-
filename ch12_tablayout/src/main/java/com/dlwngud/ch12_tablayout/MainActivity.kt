789bbb564aeb773545a9f0e9532a6e1ecdf839e2
dlwngud <wngud980411@naver.com>
2023-05-31 23:25:52 +0900

    ch12 TabLayout 정적으로 만들기

--- ch12_tablayout/src/main/java/com/dlwngud/ch12_tablayout/MainActivity.kt
+++ ch12_tablayout/src/main/java/com/dlwngud/ch12_tablayout/MainActivity.kt
@@ -10,18 +10,5 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        val tabLayout = binding.tabs
-        val tab1: TabLayout.Tab = tabLayout.newTab()
-        tab1.text = "Tab1"
-        tabLayout.addTab(tab1)
-
-        val tab2: TabLayout.Tab = tabLayout.newTab()
-        tab2.text = "Tab2"
-        tabLayout.addTab(tab2)
-
-        val tab3: TabLayout.Tab = tabLayout.newTab()
-        tab3.text = "Tab3"
-        tabLayout.addTab(tab3)
     }
 }
