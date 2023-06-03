ae144c5ea4303e17e9ec7e29cc1b2dc82c30a91c
dlwngud <wngud980411@naver.com>
2023-06-04 00:07:50 +0900

    init: ch13 Activity_Ex
    - 액티비티 클래스 하나당 <activity>태크로 등록
    - 매니페스트에 등록하지 않으면 시스템은 해당 컴포넌트를 알 수 없다.
    - android:exported : 활동이 다른 애플리케이션의 구성요소에서 시작될 수 있는지를 나타낸다.

---
+++ ch13_activity_ex/src/main/java/com/test/ch13_activity_ex/DetailActivity.kt
@@ -0,0 +1,11 @@
+package com.test.ch13_activity_ex
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+    }
+}
