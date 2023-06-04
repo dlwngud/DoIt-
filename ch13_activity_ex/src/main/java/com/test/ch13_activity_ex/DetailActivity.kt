a194f98b49d31bc850b6b050de87ca36a34dd374
dlwngud <wngud980411@naver.com>
2023-06-04 23:19:11 +0900

    feat: ch13 Activity_Ex
    - 엑스트라 데이터 추가 및 가져오기

--- ch13_activity_ex/src/main/java/com/test/ch13_activity_ex/DetailActivity.kt
+++ ch13_activity_ex/src/main/java/com/test/ch13_activity_ex/DetailActivity.kt
@@ -2,10 +2,19 @@ package com.test.ch13_activity_ex
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.test.ch13_activity_ex.databinding.ActivityDetailBinding
 
 class DetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail)
+        val binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // 엑스트라 데이터 가져오기
+        val data1 = intent.getStringExtra("data1")
+        val data2 = intent.getIntExtra("data2", 0)
+
+        binding.tv1.text = data1
+        binding.tv2.text = data2.toString()
     }
 }
--- ch13_activity_ex/src/main/java/com/test/ch13_activity_ex/MainActivity.kt
+++ ch13_activity_ex/src/main/java/com/test/ch13_activity_ex/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.test.ch13_activity_ex
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.test.ch13_activity_ex.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // MainActivity 에서 DetailActivity 를 실행할 때
+        val intent = Intent(this, DetailActivity::class.java)
+        intent.putExtra("data1", "hello")
+        intent.putExtra("data2", 10)
+        startActivity(intent)
     }
 }
