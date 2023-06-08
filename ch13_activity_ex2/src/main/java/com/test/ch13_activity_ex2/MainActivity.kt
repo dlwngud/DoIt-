15a883f87af15c1076185998046df77c15f6d876
dlwngud <wngud980411@naver.com>
2023-06-09 00:35:32 +0900

    feat: ch13 Activity_Ex02
    - Bundle을 통해 화면 회전시(액티비티 종료할 떄)에도 데이터 저장하고 복원하기

--- ch13_activity_ex2/src/main/java/com/test/ch13_activity_ex2/MainActivity.kt
+++ ch13_activity_ex2/src/main/java/com/test/ch13_activity_ex2/MainActivity.kt
@@ -1,11 +1,36 @@
 package com.test.ch13_activity_ex2
 
+import android.content.pm.ActivityInfo
+import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.test.ch13_activity_ex2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+    var count = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button.setOnClickListener {
+            count++
+            binding.textView.text = count.toString()
+        }
+
+        // 세로모드에서 가로모드로 전환할 때 데이터 유지
+        if (savedInstanceState != null) {
+            count = savedInstanceState.getInt("count",0)
+            binding.textView.text = count.toString()
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+        // 번들에 데이터 저장
+        outState.putInt("count", count)
     }
 }
