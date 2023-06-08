e24cc4df44e628c07e80b4743de56860343b2a22
dlwngud <wngud980411@naver.com>
2023-06-08 23:51:45 +0900

    init: ch13 Activity_Ex02

---
+++ ch13_activity_ex2/src/main/java/com/test/ch13_activity_ex2/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.test.ch13_activity_ex2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
