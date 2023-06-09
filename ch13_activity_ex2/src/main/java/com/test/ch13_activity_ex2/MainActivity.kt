f11e716cdcbcd47d1fd0c265181caade6a14011e
dlwngud <wngud980411@naver.com>
2023-06-09 23:16:31 +0900

    feat: 소프트 키보드 제어
    - showSoftInput(): 소프트 키보드를 화면에 나타나게 한다.
    - hideSoftInputFromWindow(): 소프트 키보드를 사라지게 한다.

--- ch13_activity_ex2/src/main/java/com/test/ch13_activity_ex2/MainActivity.kt
+++ ch13_activity_ex2/src/main/java/com/test/ch13_activity_ex2/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.pm.ActivityInfo
 import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.inputmethod.InputMethodManager
 import com.test.ch13_activity_ex2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -22,9 +23,27 @@ class MainActivity : AppCompatActivity() {
 
         // 세로모드에서 가로모드로 전환할 때 데이터 유지
         if (savedInstanceState != null) {
-            count = savedInstanceState.getInt("count",0)
+            count = savedInstanceState.getInt("count", 0)
             binding.textView.text = count.toString()
         }
+
+        val imm = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+        binding.button2.setOnClickListener {
+            // et에 포커스주고 키보드 올리기
+            binding.et.requestFocus()
+            imm.showSoftInput(binding.et, InputMethodManager.SHOW_IMPLICIT)
+        }
+
+        binding.button3.setOnClickListener {
+            // 키보드 내리기
+            if (currentFocus != null) {
+                imm.hideSoftInputFromWindow(
+                    currentFocus!!.windowToken,
+                    InputMethodManager.HIDE_NOT_ALWAYS
+                )
+            }
+        }
+
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
