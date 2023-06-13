9b457bc8f8c8e00a97e08cc98b9685c262bbed3d
dlwngud <wngud980411@naver.com>
2023-06-13 23:10:46 +0900

    feat: 브로드캐스트 리시버 동적 등록과 해제
    - registerReceiver() : 매니페스트에 <receiver>로 등록하지 않아도 시스템에서 리시버를 인지
    - unregisterReceiver() : registerReceiver()를 사용하고 필요없으면 해제

--- ch14_receiver_ex01/src/main/java/com/test/ch14_receiver_ex01/MainActivity.kt
+++ ch14_receiver_ex01/src/main/java/com/test/ch14_receiver_ex01/MainActivity.kt
@@ -1,5 +1,9 @@
 package com.test.ch14_receiver_ex01
 
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.test.ch14_receiver_ex01.databinding.ActivityMainBinding
@@ -12,5 +16,19 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        // 리시버 객체 생성
+        val receiver = object : BroadcastReceiver(){
+            override fun onReceive(context: Context?, intent: Intent?) {
+
+            }
+        }
+
+        // registerReceiver() : 매니페스트에 <receiver>로 등록하지 않아도 시스템에서 리시버를 인지
+        val filter = IntentFilter("ACTION_RECEIVER")
+        registerReceiver(receiver, filter)
+
+        // registerReceiver()를 사용하고 필요없으면 해제해 줘야한다.
+        unregisterReceiver(receiver)
     }
 }
