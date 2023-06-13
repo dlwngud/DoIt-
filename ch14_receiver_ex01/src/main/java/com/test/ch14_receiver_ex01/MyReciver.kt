282d5d78b655f2445bb731669fc31ef373d75fad
dlwngud <wngud980411@naver.com>
2023-06-13 23:05:01 +0900

    feat: 브로드캐스트 리시버 만들고 등록

---
+++ ch14_receiver_ex01/src/main/java/com/test/ch14_receiver_ex01/MyReciver.kt
@@ -0,0 +1,11 @@
+package com.test.ch14_receiver_ex01
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class MyReceiver: BroadcastReceiver() {
+    override fun onReceive(context: Context?, intent: Intent?) {
+
+    }
+}
