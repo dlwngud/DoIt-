2ff047135a5f60a54968c05298d8358bf5544cad
dlwngud <wngud980411@naver.com>
2023-06-16 00:26:22 +0900

    feat: 서비스 생성과 실행
    서비스를 실행할 때 시스템에 인텐트를 전달하는 2가지 함수가 있다.
    - startService() : 서비스가 무기한으로 실행될 수 있으며, stopSelf()를 호출하여 자체적으로 중단해야 한다. 다른 구성 요소도 stopService()를 호출하면 서비스를 중단시킬 수 있다. 서비스가 중단되면 시스템이 이를 소멸시킨다.
    
    - bindService() : 클라이언트가 IBinder 인터페이스를 통해 서비스와 통신을 주고받을 수 있다. 클라이언트가 연결을 종료하려면 unbindService()를 호출하면 된다. 여러 클라이언트가 같은 서비스에 바인딩될 수 있으며, 모든 클라이언트가 바인딩을 해제하면 시스템이 해당 서비스를 소멸시킨다. 서비스가 스스로 중단하지 않아도 된다.

--- ch15_service_ex01/src/main/java/com/test/ch15_service_ex01/MainActivity.kt
+++ ch15_service_ex01/src/main/java/com/test/ch15_service_ex01/MainActivity.kt
@@ -1,14 +1,49 @@
 package com.test.ch15_service_ex01
 
+import android.content.ComponentName
+import android.content.Context
 import android.content.Intent
+import android.content.ServiceConnection
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.IBinder
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // startService() 함수로 실행
+        // 서비스 실행
+        // val intent = Intent(this, MyService::class.java)
+        // startService(intent)
 
+        // 암시적 인텐트로 실행
+        //val intent = Intent("ACTION_OUTER_SERVICE")
+        //intent.setPackage("com.test.test_outter")
+        // startService(intent)
+
+        // 서비스 종료
+        //stopService(intent)
+
+        // bindService() 함수로 실행
+        val connection: ServiceConnection = object: ServiceConnection{
+            // bindService() 함수로 서비스를 구동할 때 자동으로 호출
+            override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+                TODO("Not yet implemented")
+            }
+
+            // unbindService() 함수로 서비스를 종료할 때 자동으로 호출
+            override fun onServiceDisconnected(name: ComponentName?) {
+                TODO("Not yet implemented")
+            }
+        }
+
+        val intent = Intent(this, MyService::class.java)
+        // 인텐트 객체, ServiceConnection을 구현한 객체, Int 타입의 flags
+        bindService(intent,connection, Context.BIND_AUTO_CREATE)
+
+        // 서비스 종료
+        unbindService(connection)
     }
 }
